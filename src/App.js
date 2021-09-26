5236a647b53e05c21d14c993bb1178b8cc4a69d4
Atharva2503 <81301059+Atharva2503@users.noreply.github.com>
2021-09-26 20:48:20 +0530

    Created react app

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div classNameName="App">
+      {
+  <div className="Container" >
+  <div className="chatlist">
+   
+      <div className="chatperson"><h3 id="Name">Lorem ipsum.</h3></div>
+      <div className="chatperson"><h3 id="Name">Lorem ipsum.</h3></div>
+      <div className="chatperson"><h3 id="Name">Lorem ipsum.</h3></div>
+  </div>
+  <div className="Chat">
+      
+      <div className="recieved">
+        <p>HI Atharva</p>
+      </div>
+      <div className="sent">
+        <p>HI Tanmay</p>
+      </div>
+      
+          </div>
+          </div>
+      }
+     </div>
+  );
+}
+
+export default App;
